492f20b2c71a101bba3bd04237b35aab7bda4129
iwakura1ain <ernie937@gmail.com>
2023-11-22 00:42:28 +0900

    ...

--- FinancierBackend/FinancierBackend/FinancierBackend/settings.py
+++ FinancierBackend/FinancierBackend/FinancierBackend/settings.py
@@ -80,13 +80,25 @@ TEMPLATES = [
 WSGI_APPLICATION = 'FinancierBackend.wsgi.application'
 
 
+# default
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'financiar',
+        'USER': 'testusr',
+        'PASSWORD': '1234',
+        'HOST':'database',
+        'PORT':3306,
     }
 }
 
